671b87c783a34a27f06a04955977d5e9cbe03782
Heshan Eranga <erangamx@gmail.com>
2023-02-04 20:14:35 +0530

    event handler configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSseConsumer from './lib';
+import { EventHandlers, EventListeners } from './lib/types';
+
+const eventHandlers: EventHandlers = {
+  onerror: (event: Event) => {
+    console.error('Error Occurred', event);
+  },
+  onopen: (event: Event) => {
+    console.log('Connection Opened', event);
+  },
+  onmessage: (messageEvent: MessageEvent) => {
+    console.log('Message received', messageEvent);
+  },
+};
 
 const App = () => {
-  const consumeEvent = (e: Event) => {
-    console.log(e);
+  const [data, setData] = useState(null);
+
+  const eventListeners: EventListeners = {
+    message: (messageEvent: MessageEvent) => {
+      console.log('Received "message" event:', messageEvent);
+      setData(messageEvent?.data);
+    },
+    ping: (messageEvent: MessageEvent) => {
+      console.log('Received "ping" event:', messageEvent);
+      setData(messageEvent?.data);
+    },
   };
 
-  const testEventConsumer = useSseConsumer('', {}, consumeEvent);
+  const testEventConsumer: EventSource | null = useSseConsumer(
+    `http://localhost:5000/notify`,
+    {},
+    eventHandlers,
+    eventListeners,
+  );
 
-  console.log(testEventConsumer);
+  const handleClose = () => {
+    testEventConsumer?.close();
+  };
 
-  return <div>React SSE Consumer...</div>;
+  return (
+    <div>
+      <div>React SSE Consumer...</div>
+      <div>Data: {data}</div>
+      <button type="button" onClick={handleClose}>
+        Close
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,33 @@
-export type ConsumeEvent = {
-  (event: Event): void;
-};
+export type EventHandler = (event: Event) => void;
+
+export type MessageEventHandler = (event: MessageEvent) => void;
+
+interface Handlers {
+  [key: string]: EventHandler | MessageEventHandler | undefined;
+}
+
+export interface EventHandlers extends Handlers {
+  /**
+   * Fired when a connection to an event source failed to open.
+   */
+  onerror?: EventHandler;
+  /**
+   * Fired when a connection to an event source has opened.
+   */
+  onopen?: EventHandler;
+  /**
+   * Fired when data is received from an event source.
+   */
+  onmessage?: MessageEventHandler;
+}
+
+export interface EventListeners extends Handlers {
+  /**
+   * Capture events without an event field as well as events that have the specific type
+   */
+  message: MessageEventHandler;
+  /**
+   * Capture events with specific event type
+   */
+  [key: string]: MessageEventHandler;
+}
